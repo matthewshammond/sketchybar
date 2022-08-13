d61b7525393deeec1f7628f631a229c535540e80
Matthew Hammond <65371724+matthewshammond@users.noreply.github.com>
2022-08-13 17:09:10 -0400

    fixed wifi label updating

--- plugins/wifi.sh
+++ plugins/wifi.sh
@@ -4,3 +4,5 @@ source "$HOME/.config/sketchybar/colors.sh"
 
 CURRENT_WIFI="$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I)"
 SSID="$(echo "$CURRENT_WIFI" | grep -o "SSID: .*" | sed 's/^SSID: //')"
+
+sketchybar --set "Control Center,WiFi" label="${SSID}"
